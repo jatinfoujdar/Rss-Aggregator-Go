cd9d8c58bc4e29fa043618cb3f52a8dbdbe6814e
Jatin Foujdar <jatin321001@gmail.com>
2025-08-13 01:37:18 +0530

    fix: correct SQL syntax for users table creation and deletion

--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -1,12 +1,10 @@
 -- +goose Up
-CREATE TABLE users{
+CREATE TABLE users(
     id UUID PRIMARY KEY,
-    created_at Timestamp NOT NULL ,
-    updated_at Timestamp NOT NULL,
-    name text NOT NULL,
-}
-
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    name text NOT NULL
+);
 
 -- +goose Down
-
-DRoP TABLE users;
+DROP TABLE users;

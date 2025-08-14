658bce82b3d7c6275f44b8a4699452bf09104f5a
Jatin Foujdar <jatin321001@gmail.com>
2025-08-14 19:06:44 +0530

    feat: enhance user creation handler with JSON parsing and error handling

--- handler_user.go
+++ handler_user.go
@@ -1,10 +1,33 @@
 package main
 
 import (
-	"net/http"
+	"encoding/json"
+	"fmt"
 	
+	"net/http"
+
+	"github.com/google/uuid"
+	"github.com/jatinfoujdar/Rss-Aggregator-Go/internal/database"
 )
 
 func (apiCfg *apiConfig)handlerCreateUser(w http.ResponseWriter, r *http.Request){
+
+type parameters struct{
+	Name string `json:"name"`
+}
+decoder :=  json.NewDecoder(r.Body)
+params := parameters{}
+// decoder.Decode(&params)
+err := decoder.Decode(&params)
+if err != nil {
+	respondWithError(w,400, fmt.Sprintf("Error parsing JSON: %v", err))
+return 
+}
+
+apiCfg.DB.CreateUser(r.Context(), database.CreateUserParams{
+	ID: uuid.New(),
+	Name: params.Name,
+
+})
 	respondWithJson(w,200, struct{}{})
 }

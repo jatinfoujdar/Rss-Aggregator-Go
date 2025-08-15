5565fdb6fbe43a3e8b62d4da1a3193fd207714d3
Jatin Foujdar <jatin321001@gmail.com>
2025-08-15 18:13:39 +0530

    fix: improve formatting and structure in user creation handler

--- handler_user.go
+++ handler_user.go
@@ -1,33 +1,33 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	
-	"net/http"
+    "encoding/json"
+    "fmt"
+    "net/http"
+    "time"
 
-	"github.com/google/uuid"
-	"github.com/jatinfoujdar/Rss-Aggregator-Go/internal/database"
+    "github.com/google/uuid"
+    "github.com/jatinfoujdar/Rss-Aggregator-Go/internal/database"
 )
 
-func (apiCfg *apiConfig)handlerCreateUser(w http.ResponseWriter, r *http.Request){
+func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Request) {
+    type parameters struct {
+        Name string `json:"name"`
+    }
+    decoder := json.NewDecoder(r.Body)
+    params := parameters{}
+    err := decoder.Decode(&params)
+    if err != nil {
+        respondWithError(w, 400, fmt.Sprintf("Error parsing JSON: %v", err))
+        return
+    }
 
-type parameters struct{
-	Name string `json:"name"`
-}
-decoder :=  json.NewDecoder(r.Body)
-params := parameters{}
-// decoder.Decode(&params)
-err := decoder.Decode(&params)
-if err != nil {
-	respondWithError(w,400, fmt.Sprintf("Error parsing JSON: %v", err))
-return 
-}
+    user := apiCfg.DB.CreateUser(r.Context(), database.CreateUserParams{
+        ID:        uuid.New(),
+        CreatedAt: time.Now().UTC(),
+        UpdatedAt: time.Now().UTC(),
+        Name:      params.Name,
+    })
 
-apiCfg.DB.CreateUser(r.Context(), database.CreateUserParams{
-	ID: uuid.New(),
-	Name: params.Name,
-
-})
-	respondWithJson(w,200, struct{}{})
+respondWithJson(w, 200, user) 
 }
